bdfc5582f4b5fbed75b6a86f039e65784c19e9b4
SeongUk18 <adulty22@gmail.com>
2024-11-10 19:04:41 +0900

    Feat: 백준 문제 재풀이

--- Baekjoon/Silver1/BOJ2667.py
+++ Baekjoon/Silver1/BOJ2667.py
@@ -1,49 +1,54 @@
-import sys
 from collections import deque
 
-n = int(sys.stdin.readline())
+n = int(input())
 
 map_list = []
-visited = [[True for _ in range(n)] for _ in range(n)]
-dx = (0, 0, -1, 1)
-dy = (-1, 1, 0, 0)
-number = 1
 
 for _ in range(n):
-    map_list.append(list(sys.stdin.readline().strip()))
+    map_list.append(list(map(int, input().strip())))
+
+visited = [[False for _ in range(n)] for _ in range(n)]
+
+x_list = [0, 0, -1, 1]
+y_list = [-1, 1, 0, 0]
 
 
-def bfs(start_y, start_x, number):  # 단지 번호를 위한 3번째 인자
+def bfs(nx, ny, n):
     q = deque()
-    q.append((start_y, start_x))
+    q.append((nx, ny))
+    visited[nx][ny] = True
+    area = 1
 
     while q:
-        y, x = q.popleft()
-        if not visited[y][x]:
-            continue
-        map_list[y][x] = number
-        visited[y][x] = False
-        for k in range(4):
-            ny = y + dy[k]
-            nx = x + dx[k]
-            if 0 <= ny < n and 0 <= nx < n and visited[ny][nx] and map_list[ny][nx] == "1":
-                q.append((ny, nx))
+        x, y = q.popleft()
 
+        for k in range(4):
+            cur_x = x + x_list[k]
+            cur_y = y + y_list[k]
+
+            if (
+                0 <= cur_x < n
+                and 0 <= cur_y < n
+                and visited[cur_x][cur_y] == False
+                and map_list[cur_x][cur_y] == 1
+            ):
+                area += 1
+                q.append((cur_x, cur_y))
+                visited[cur_x][cur_y] = True
+
+    return area
+
+
+count = 0
+answer = []
 
 for i in range(n):
     for j in range(n):
-        if map_list[i][j] == "1" and visited[i][j]:
-            bfs(i, j, number)
-            number += 1
+        if map_list[i][j] == 1 and visited[i][j] == False:
+            count += 1
+            answer.append(bfs(i, j, n))
 
-# print(map_list)
-print(number - 1)  # 단지 번호가 마지막에 1 증가 되어 1 빼줌
-count = {home: 0 for home in range(1, number)}  # 단지 갯수 확인을 위한 dic
-for m_list in map_list:  # 단지 갯수 확인
-    for check in m_list:
-        if check in count:
-            count[check] += 1
-count_val = list(count.values())  # 단지 수가 적은 순으로 정렬
-count_val.sort()
-for i in count_val:  # 결과 출력용
+answer.sort()
+print(count)
+for i in answer:
     print(i)
